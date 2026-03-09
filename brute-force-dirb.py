7e0949ade60c6b049e368e876e2628221e8f1652
Leandro Carvalho <151436121+LeandroMCarv@users.noreply.github.com>
2026-03-09 20:31:23 -0300

    feat: tratamento de erros durante a execucao

--- brute-force-dirb.py
+++ brute-force-dirb.py
@@ -3,11 +3,19 @@ import requests
 
 def brute(site,wordlist):
     for word in wordlist:
-        site_final = "{}/{}".format(site,word.strip())
-        response = requests.get(site_final)
-        code = response.status_code
-        if code != 404:
-            print("{} -- {}".format(site_final,code))
+        try:
+            site_final = "{}/{}".format(site,word.strip())
+            response = requests.get(site_final)
+            code = response.status_code
+            if code != 404:
+                print("{} -- {}".format(site_final,code))
+        
+        except KeyboardInterrupt:
+            sys.exit(0)
+        
+        except Exception as error:
+            print(error)
+            pass
 
 if __name__ == "__main__":
     site = sys.argv[1]
